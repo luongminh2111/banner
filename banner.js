756c4a48d83dbd5f72a7f99dc8eeab35ade90433
luongminh2111 <79582356+luongminh2111@users.noreply.github.com>
2022-05-07 09:17:31 +0700

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,6 +1,18 @@
-import axios from "axios";
-function SapoWeb(sectionID) {
-    axios.get("http://localhost:8080/api/banner-status/random/" + sectionID).then(res => {
-        console.log("data random  lay ra: ", res.data)
-    })
+const uri = 'http://localhost:8080/api/banner-status/random/';
+
+function getImg(sectionID) {
+    fetch(uri + sectionID)
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+        // .then(data => console.log("data: " + data.imgUrl))
+        .catch(error => console.error('Unable to get items.', error));
+
+}
+var img = new Image();
+var a = new a();
+function _displayItems(data) {
+    const tBody = document.getElementById('img');
+    tBody.appendChild(img);
+    img.src = data.imgUrl;
+
 }
